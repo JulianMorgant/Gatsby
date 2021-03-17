e4b277e45934e09e9a0253f2d0193001d8875d59
Julian MORGANT <julian.morgant@yahoo.fr>
2021-03-17 18:04:03 +0100

    refactoring routes / serices

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+
+/*
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
+*/
+
 require("dotenv").config();
 
-const express = require("express");
+const express      = require('express');
+const cookieParser = require('cookie-parser');
+const logger       = require('morgan');
+const cors         = require('cors');
 const User = require("./models/User");
-const app = express();
 const mongoose = require("mongoose");
+const indexRouter = require('./routes/v1/index');
+
+const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
@@ -13,28 +42,26 @@ db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connection to db established"));
 app.use(express.json());
 
-const usersRouter = require("./routes/users");
-app.use("/users", usersRouter);
 
-// const NewUser = mongoose.model("NewUser", userSchema);
 
-const testUser = new User({ email: "mail1", psw: "psw3" });
+app.use(cors({
+    exposedHeaders: ['Authorization'],
+    origin: '*'
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-console.log(testUser.email);
-console.log(testUser.psw); 
+app.use('/v1', indexRouter);
 
-/*
-testUser.save(function (err, testUser){
-    if (err) return console.error(err);
-    console.log(' save => ' + testUser.email + ' ' + testUser.psw)
-}) 
-*/
-
-User.find(function (err, users) {
-  if (err) return console.error(err);
-  console.log(users);
+app.use(function(req, res, next) {
+    res.status(404).json({name: 'API', version: '1.0', status: 404, message: 'not_found'});
 });
+
+//module.exports = app;
 
 app.listen(process.env.PORT, () =>
   console.log(`server has started at port ${process.env.PORT}`)
 );
+
---
+++ index_old.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const express = require("express");
+const User = require("./models/User");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("connection to db established"));
+app.use(express.json());
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+// const NewUser = mongoose.model("NewUser", userSchema);
+
+const testUser = new User({ email: "mail1", psw: "psw3" });
+
+console.log(testUser.email);
+console.log(testUser.psw); 
+
+/*
+testUser.save(function (err, testUser){
+    if (err) return console.error(err);
+    console.log(' save => ' + testUser.email + ' ' + testUser.psw)
+}) 
+*/
+
+User.find(function (err, users) {
+  if (err) return console.error(err);
+  console.log(users);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`server has started at port ${process.env.PORT}`)
+);
