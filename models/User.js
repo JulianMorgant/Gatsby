e72cbea532e062dc51cfa7a9d99052eb65ad994d
Julian MORGANT <julian.morgant@yahoo.fr>
2021-03-23 15:29:22 +0100

    implement data validation sync and async

--- models/User.js
+++ models/User.js
@@ -1,34 +1,86 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
+const services = require("../services/v1/user");
+
+// upgrade validation : https://mongoosejs.com/docs/api.html#schematype_SchemaType-validate
 
 const User = new Schema(
   {
     name: {
       type: String,
       trim: true,
-      required: [false],
+      required: [true],
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z]{2,15}$/.test(v);
+        },
+        message: (props) => `${props.value} n'est pas un nom valide`,
+      },
     },
     firstname: {
       type: String,
       trim: true,
       required: [false],
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z]{2,15}$/.test(v);
+        },
+        message: (props) => `${props.value} n'est pas un prénom valide`,
+      },
     },
     pseudo: {
       type: String,
       trim: true,
       required: [true, "Le pseudo est obligatoire"],
+      // unique: true, //TODO https://mongoosejs.com/docs/validation.html#the-unique-option-is-not-a-validator
+      validate: {
+ 
+        //async validation for unique pseudo require a promise
+        validator: async function (v) { 
+          var ret = await services.isPseudoExists(v);
+          return !ret;
+        
+        },
+          
+       
+        message: (props) => `${props.value} est déja utilisé`,
+
+
+    /*    validator: function (v) {
+          console.log("------------");
+          return /^[a-zA-Z0-9]{4,15}$/.test(v);
+        },
+        message: (props) => `${props.value} n'est pas un pseudo valide`,
+        */
+      },
+      
     },
+
     email: {
       type: String,
       trim: true,
       required: [true, "L’email est obligatoire"],
       unique: true, // index unique //TODO Validation : https://mongoosejs.com/docs/validation.html#the-unique-option-is-not-a-validator
       lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+            v
+          );
+        },
+        message: (props) => `${props.value} n'est pas un email valide`,
+      },
     },
     password: {
       type: String,
-      trim: true,
+      trim: true, // TODO ???
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9]{1,15}$/.test(v);
+        },
+        message: (props) => `${props.value} n'est pas un email valide`,
+      },
     },
     roles: {
       type: [String],
